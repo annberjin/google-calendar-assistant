cddf280d4c7a05aecb6e626677eb603909b28cc2
Ethan Cabalona <cabalona.e@northeastern.edu>
2026-06-16 16:29:37 -0400

    added update and delete endpoints for google calendar

--- calendar.js
+++ calendar.js
@@ -30,3 +30,62 @@ export async function getEvents(auth, start, end) {
     end: e.end?.dateTime,
   }));
 }
+
+/**
+ * Update an event with a given id.
+ * Specify the new title, start and end time, or use the existing values from a previous query if you want to keep them the same.
+ */
+export async function updateEvent(
+  auth,
+  eventId,
+  title,
+  description,
+  startDateTime,
+  endDateTime,
+) {
+  const calendar = google.calendar({
+    version: "v3",
+    auth,
+  });
+
+  const res = await calendar.events.update({
+    calendarId: "primary",
+    eventId: eventId,
+    start: {
+      dateTime: startDateTime,
+    },
+    end: {
+      dateTime: endDateTime,
+    },
+    description: description,
+    summary: title,
+    title: title,
+  });
+
+  if (res.status !== 200) {
+    return `Error updating event: ${res.statusText}`;
+  } else {
+    return "Event updated successfully";
+  }
+}
+
+/**
+ * Delete an event with a given id.
+ */
+export async function deleteEvent(auth, eventId) {
+  const calendar = google.calendar({
+    version: "v3",
+    auth,
+  });
+
+  const res = await calendar.events.delete({
+    calendarId: "primary",
+    eventId,
+  });
+
+  if (res.status !== 200) {
+    return `Error deleting event: ${res.statusText}`;
+  } else {
+    return "Event deleted successfully";
+  }
+}
